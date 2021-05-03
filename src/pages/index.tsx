a0f33f867bc4404a444d6a3afbde2b89a43abd51
Vinícius Baptista de Mello <viniciusbmello@gmail.com>
2021-05-03 02:38:38 -0300

    SWR ans Axios fetch util

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,41 @@
 import React from 'react';
 import { NextPage } from 'next';
+import useRequest from '../utils/useRequest';
 
-const Home: NextPage = () => <h1>Teste</h1>;
+interface ITools {
+  id: number;
+  title: string;
+  link: string;
+  description: string;
+  tags: Array<string>;
+}
+
+const Home: NextPage = () => {
+  const { data, error } = useRequest<ITools[]>({
+    method: 'get',
+    url: 'http://localhost:3333/tools',
+  });
+
+  if (error) return <div>failed...</div>;
+
+  return (
+    <>
+      <ul>
+        {data
+          ? data.map(tool => (
+              <li key={tool.id}>
+                <p>{tool.title}</p>
+                <p>{tool.link}</p>
+                <p>{tool.description}</p>
+                <p>
+                  {tool.tags ? tool.tags.map(tag => <span>#{tag} </span>) : ''}
+                </p>
+              </li>
+            ))
+          : 'loading...'}
+      </ul>
+    </>
+  );
+};
 
 export default Home;
